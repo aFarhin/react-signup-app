667432bb925edf77ff0479e0e204e29b04ff38dc
aFarhin <ahmeda.farhin786@gmail.com>
2023-05-27 23:09:28 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUpForm from "./Components/SignUpForm";
+import Profile from "./Components/Profile";
+import Navbar from "./Components/Navbar";
+
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SignUpForm />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
     </div>
   );
 }
